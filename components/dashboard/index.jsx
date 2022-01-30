8d4cef7e0c55f5be1343d82fcfb5b1c2c512710c
OverAcheiever <75264797+OverAcheiever@users.noreply.github.com>
2022-01-31 03:06:06 +0530

    1

--- components/dashboard/index.jsx
+++ components/dashboard/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Leaderboard from "./Leaderboard";
+import Graph from "./Graph";
 import getLeaderboard from "./api/getLeaderboard";
 
 export default function Dashboard() {
   const [leaderboard, setLeaderboard] = useState(null);
+  const [graphData, setGraphData] = useState(null);
 
   useEffect(() => {
     if (!leaderboard) {
@@ -17,6 +19,7 @@ export default function Dashboard() {
       <Sidebar />
       <div className="w-full h-full p-3 flex gap-x-3 bg-gray-100">
         <Leaderboard leaderboard={leaderboard} />
+        <Graph />
       </div>
     </div>
   );
